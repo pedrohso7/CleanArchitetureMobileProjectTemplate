f16138919c86307250f1c434bdab4255de1601fc
Pedro Oliveira <pholiveira1998@hotmail.com>
2022-10-01 18:44:57 -0300

    fix(splash): fix splash compilation error.

--- lib/features/splash/splash_bindings.dart
+++ lib/features/splash/splash_bindings.dart
@@ -1,3 +1,5 @@
+import 'package:answer_me_app/features/authentication/data/datasources/local_datasource.dart';
+import 'package:answer_me_app/features/authentication/data/datasources/remote_datasource.dart';
 import 'package:answer_me_app/features/authentication/data/repositories/user_repository.dart';
 import 'package:answer_me_app/features/authentication/domain/repositories/user_repository_interface.dart';
 import 'package:answer_me_app/features/authentication/domain/usecases/is_user_logged.dart';
@@ -7,6 +9,17 @@ import 'splash_controller.dart';
 class SplashBindings implements Bindings {
   @override
   void dependencies() {
+    Get.lazyPut<LocalDataSourceInterface>(
+      () => LocalDataSource(getStorage: Get.find()),
+    );
+    Get.lazyPut<RemoteDataSourceInterface>(
+      () => RemoteDataSource(remoteClient: Get.find()),
+    );
+    Get.lazyPut<UserRepositoryInterface>(
+      () => UserRepository(
+          remoteDataSource: Get.find(), localDataSource: Get.find()),
+    );
+
     Get.lazyPut<UserRepositoryInterface>(
       () => UserRepository(
           remoteDataSource: Get.find(), localDataSource: Get.find()),
