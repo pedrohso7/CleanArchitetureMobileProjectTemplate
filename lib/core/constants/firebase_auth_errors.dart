101502cb0c01db57957363efc17e812ed3062936
Pedro Oliveira <pholiveira1998@hotmail.com>
2022-10-24 16:02:32 -0300

    refactor(authentication): refactor authentication endpoints to be according to firebase authentication provider.

---
+++ lib/core/constants/firebase_auth_errors.dart
@@ -0,0 +1,13 @@
+class AuthErrors {
+  AuthErrors._();
+  static const Map<String, String> firebase_auth_errors = {
+    "EMAIL_EXISTS": "Já existe um conta utilizando este email",
+    "OPERATION_NOT_ALLOWED": "Operação não permitida",
+    "TOO_MANY_ATTEMPTS_TRY_LATER":
+        "Acesso bloqueado temporariamente devido ao número de tentativas",
+    "EMAIL_NOT_FOUND": "Já existe um conta utilizando este email",
+    "WEAK_PASSWORD": "Sua senha deve conter pelo menos 6 caracteres",
+    "INVALID_PASSWORD": "Senha incorreta",
+    "USER_DISABLED": "O usuário foi desabilitado por algum administrador",
+  };
+}
--- lib/features/authentication/data/datasources/auth_remote_datasource.dart
+++ lib/features/authentication/data/datasources/auth_remote_datasource.dart
@@ -1,5 +1,7 @@
+import 'package:clean_architeture_project/core/constants/firebase_auth_errors.dart';
 import 'package:clean_architeture_project/core/errors/remote_client_exception.dart';
 import 'package:clean_architeture_project/core/platform/remote_client.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 
 abstract class AuthRemoteDataSourceInterface {
@@ -25,18 +27,17 @@ class AuthRemoteDataSource implements AuthRemoteDataSourceInterface {
     required String email,
     required String password,
   }) async {
-    final Response result = await remoteClient.post(
-      '/auth/register',
-      {
-        'name': name,
-        'email': email,
-        'password': password,
-      },
-    );
+    final Response result = await remoteClient.post('/accounts:signUp', {
+      'email': email,
+      'password': password,
+      'returnSecureToken': true,
+    }, query: {
+      'key': dotenv.env['FIREBASE_WEB_KEY'],
+    });
 
     if (result.hasError) {
       throw RemoteClientException(
-        '${result.body['error']}',
+        '${AuthErrors.firebase_auth_errors[result.body['error']['message'].split(' ')[0]]}',
         code: result.statusCode,
       );
     }
@@ -49,17 +50,21 @@ class AuthRemoteDataSource implements AuthRemoteDataSourceInterface {
     required String email,
     required String password,
   }) async {
-    final Response result = await remoteClient.get(
-      '/auth/login',
-      query: {
+    final Response result = await remoteClient.post(
+      '/accounts:signInWithPassword',
+      {
         'email': email,
         'password': password,
+        'returnSecureToken': true,
+      },
+      query: {
+        'key': dotenv.env['FIREBASE_WEB_KEY'],
       },
     );
 
     if (result.hasError) {
       throw RemoteClientException(
-        '${result.body['error']}',
+        '${AuthErrors.firebase_auth_errors[result.body['error']['message']]}',
         code: result.statusCode,
       );
     }
