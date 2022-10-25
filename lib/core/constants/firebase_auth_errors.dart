5334f280b935fbe1b7acead92dc41a313bbb1a8f
Pedro Oliveira <pholiveira1998@hotmail.com>
2022-10-25 10:14:54 -0300

    fix(errors): fix auth error message.

--- lib/core/constants/firebase_auth_errors.dart
+++ lib/core/constants/firebase_auth_errors.dart
@@ -5,7 +5,7 @@ class AuthErrors {
     "OPERATION_NOT_ALLOWED": "Operação não permitida",
     "TOO_MANY_ATTEMPTS_TRY_LATER":
         "Acesso bloqueado temporariamente devido ao número de tentativas",
-    "EMAIL_NOT_FOUND": "Já existe um conta utilizando este email",
+    "EMAIL_NOT_FOUND": "Email não cadastrado",
     "WEAK_PASSWORD": "Sua senha deve conter pelo menos 6 caracteres",
     "INVALID_PASSWORD": "Senha incorreta",
     "USER_DISABLED": "O usuário foi desabilitado por algum administrador",
