c59319e4a6a6b9cbc52d27ffec3181f737572373
Pedro Oliveira <pholiveira1998@hotmail.com>
2023-08-01 17:24:45 -0400

    refactor(assets): move assets to core path and remove unused images.

--- lib/core/constants/image_path_constants.dart
+++ lib/core/constants/image_path_constants.dart
@@ -1,4 +1,4 @@
 class ImagePathConstants {
   ImagePathConstants._();
-  static const String BRAIN_LOGO_PATH = 'lib/assets/images/brain.png';
+  static const String BRAIN_LOGO_PATH = 'lib/core/assets/images/brain.png';
 }
